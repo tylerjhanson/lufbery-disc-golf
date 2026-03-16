e86192dd70c1af85ce63c6567e935bf214fee3c4
thanson131313 <tylerjhanson@gmail.com>
2026-03-16 18:23:44 -0400

    Add Astro Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,17 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  site: "https://lufberydiscgolf.com",
+  integrations: [sitemap()],
+  adapter: cloudflare(),
+// @ts-check
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://lufberydiscgolf.com",
   integrations: [sitemap()],
