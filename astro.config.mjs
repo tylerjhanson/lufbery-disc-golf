1edb297ea36bc956ace6eae0d72a59c8a0bee150
tylerjhanson <tylerjhanson@gmail.com>
2026-03-16 19:39:41 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://lufberydiscgolf.com",
-  output: "server",
   integrations: [sitemap()],
-  adapter: cloudflare(),
 });
