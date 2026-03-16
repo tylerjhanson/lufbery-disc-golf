4e7785bc2e0da85f1fec08ebb1e78d3632f4d410
thanson131313 <tylerjhanson@gmail.com>
2026-03-16 18:31:48 -0400

    Fix astro config syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
-import cloudflare from "@astrojs/cloudflare";
-
-export default defineConfig({
-  site: "https://lufberydiscgolf.com",
-  integrations: [sitemap()],
-  adapter: cloudflare(),
-// @ts-check
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   site: "https://lufberydiscgolf.com",
   integrations: [sitemap()],
