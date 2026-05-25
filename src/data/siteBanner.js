ec878f23d4a6e5afc00b2c4c36331b4d9180ed79
tylerjhanson <tylerjhanson@gmail.com>
2026-05-25 08:57:13 -0400

    Update siteBanner.js

--- src/data/siteBanner.js
+++ src/data/siteBanner.js
@@ -2,11 +2,11 @@ export const siteBanner = {
   enabled: true,
 
   // Change this ID whenever you want dismissed visitors to see the banner again.
-  id: "2026-05-24-player-history-feature-updated-3",
+  id: "2026-05-24-player-history-feature-updated-4",
 
   label: "Update",
   message:
-    "Click your name to view your league history, including scores, tags, wins, aces, hole-by-hole details, and more. All league rounds since 2021 the original layout are included.",
+    "Click your name to view your league history, including scores, tags, wins, aces, hole-by-hole details, and more. All league rounds since 2021 on the original layout are included.",
 
   dismissible: true,
 };
