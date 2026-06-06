4cb2cd74e7d9b4e6154ad0d37e4c31582e298683
tylerjhanson <tylerjhanson@gmail.com>
2026-06-05 21:11:39 -0400

    Load UDisc score chip styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import siteBanner from "./src/integrations/siteBanner.mjs";
 import handicapHelp from "./src/integrations/handicapHelp.mjs";
+import udiscScoreStyles from "./src/integrations/udiscScoreStyles.mjs";
 
 export default defineConfig({
   site: "https://lufberydiscgolf.com",
-  integrations: [sitemap(), siteBanner(), handicapHelp()],
+  integrations: [sitemap(), siteBanner(), handicapHelp(), udiscScoreStyles()],
 });
