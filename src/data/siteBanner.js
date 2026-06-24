fbbf95dd54d132d457f5dfdfede1a2e72581a692
tylerjhanson <tylerjhanson@gmail.com>
2026-06-24 13:13:29 -0400

    Update siteBanner.js

--- src/data/siteBanner.js
+++ src/data/siteBanner.js
@@ -2,7 +2,7 @@ export const siteBanner = {
   enabled: true,
 
   // Change this ID whenever you want dismissed visitors to see the banner again.
-  id: "2026-05-24-player-history-feature-updated-7",
+  id: "2026-06-24",
 
   label: "Reminder",
   message:
