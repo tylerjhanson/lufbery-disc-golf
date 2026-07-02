45c3b6d812b0976529b4fcbd9db777e70f0b97d0
tylerjhanson <tylerjhanson@gmail.com>
2026-07-02 10:52:48 -0400

    Update siteBanner.js

--- src/data/siteBanner.js
+++ src/data/siteBanner.js
@@ -2,7 +2,7 @@ export const siteBanner = {
   enabled: true,
 
   // Change this ID whenever you want dismissed visitors to see the banner again.
-  id: "2026-06-24",
+  id: "2026-07-02",
 
   label: "Reminder",
   message:
