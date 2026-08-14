928d628e6cf8743f5f6df2b6cf516766bb8843b9
tylerjhanson <tylerjhanson@gmail.com>
2026-08-13 20:28:19 -0400

    Update siteBanner.js

--- src/data/siteBanner.js
+++ src/data/siteBanner.js
@@ -1,12 +1,12 @@
 export const siteBanner = {
-  enabled: false,
+  enabled: true,
 
   // Change this ID whenever you want dismissed visitors to see the banner again.
-  id: "2026-07-02",
+  id: "2026-08-13",
 
   label: "Reminder",
   message:
-    "3rd Annual Lufbery July 4 League. 18 holes on the original layout with a 9 a.m. shotgun start followed by an additional 9 holes on alternate holes with reshuffled cards and 8 cash CTPs",
+    "Starting Thursday, August 20, league begins at 5 p.m. Last league night will be Thursday, September 3.",
 
   dismissible: true,
 };
