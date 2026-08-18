d0f9fad9572e2d6597dd0e62661c874dca588063
tylerjhanson <tylerjhanson@gmail.com>
2026-08-18 08:32:14 -0400

    Update siteBanner.js

--- src/data/siteBanner.js
+++ src/data/siteBanner.js
@@ -2,11 +2,11 @@ export const siteBanner = {
   enabled: true,
 
   // Change this ID whenever you want dismissed visitors to see the banner again.
-  id: "2026-08-13",
+  id: "2026-08-18",
 
   label: "Reminder",
   message:
-    "Starting Thursday, August 20, league begins at 5 p.m. Last league night will be Thursday, September 3.",
+    "Starting Thursday, August 20, league begins at 5 p.m. Last singles league night of 2026 will be Thursday, September 3.",
 
   dismissible: true,
 };
