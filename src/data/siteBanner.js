67e5d2a9d19460810ffdeaf7e680320ecaf9602e
tylerjhanson <tylerjhanson@gmail.com>
2026-08-27 20:51:06 -0400

    Update siteBanner.js

--- src/data/siteBanner.js
+++ src/data/siteBanner.js
@@ -4,9 +4,9 @@ export const siteBanner = {
   // Change this ID whenever you want dismissed visitors to see the banner again.
   id: "2026-08-20",
 
-  label: "Reminder",
+  label: "Last singles league night",
   message:
-    "Last singles league night on Thursday, September 3 at 5 pm! Ace pot begins at $188.",
+    "Thursday, September 3 at 5 p.m. Ace pot begins at $188.",
 
   dismissible: true,
 };
